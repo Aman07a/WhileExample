0a06b7d2c31c5f0797efa22f5a3ff4bb6409b70f
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-18 09:51:21 +0100

    While, Do-While

--- WhileExample/Program.cs
+++ WhileExample/Program.cs
@@ -5,6 +5,36 @@ namespace WhileExample
 	{
 		static void Main(string[] args)
 		{
+			// 1 to 10
+			int i = 1;
+			do
+			{
+				System.Console.WriteLine(i + "");
+				i++;
+			}
+			while (i <= 10);
+			System.Console.WriteLine();
+
+			// 0 to 9
+			i = 0;
+			do
+			{
+				System.Console.WriteLine(i + "");
+				i++;
+			}
+			while (i < 10);
+			System.Console.WriteLine();
+
+			// 9 to 0
+			i = 9;
+			do
+			{
+				System.Console.WriteLine(i + "");
+				i--;
+			}
+			while (i >= 0);
+
+			System.Console.ReadKey();
 		}
 	}
 }
